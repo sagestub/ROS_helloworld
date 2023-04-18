acaa1a44e3a6aa538384ee26c4152e2ce9c85bd0
Sage Stubbs <sagestubbs@gmail.com>
2023-04-18 04:41:34 -0400

    working nodes with hardcoded dt=0.1, but control is unstable

--- exp2_ws/src/waypoint_follower/robotNode.py
+++ exp2_ws/src/waypoint_follower/robotNode.py
@@ -22,16 +22,19 @@ def robotPoseCallback(twistMsg):
     print("robotNode: callback updating pose")
     global posePub
     global pose_msg
+    dt = 0.1
 
-    
     #calculate updated position
-    dt = (currentTime-lastTime).to_sec()
-
-    #use received twist message to update position values:
-    pose_msg.x += twistMsg.linear.x*dt
-    pose_msg.y += twistMsg.linear.y*dt
-    pose_msg.z = 0.0
-    pose_msg.w += twistMsg.angular.z*dt
+    if pose_msg is None: #if first time executing
+        #give initial pose values:
+        pose_msg = Quaternion(x=0.0, y=0.0, z=0.0, w=0.0)
+    
+    else:
+        #use received twist message to update position values:
+        pose_msg.x += twistMsg.linear.x*dt
+        pose_msg.y += twistMsg.linear.y*dt
+        pose_msg.z = 0.0
+        pose_msg.w += twistMsg.angular.z*dt
 
     #publish the updated pose
     posePub.publish(pose_msg)
@@ -41,27 +44,14 @@ def main():
     global posePub
     global pose_msg
     print("robotNode: executing main()")
-    rate=rospy.Rate(10)
-    while not rospy.is_shutdown():
-        # print(pose_msg)
-        if pose_msg is None: #if first time executing
-            #give initial pose values:
-            pose_msg = Quaternion(x=0.0, y=0.0, z=0.0, w=0.0)
-            # pose_msg.x = 0.0
-            # pose_msg.y = 0.0
-            # pose_msg.z = 0.0
-            # pose_msg.w = 0.0
-            posePub.publish(Quaternion(x=0.0, y=0.0, z=0.0, w=0.0))
-            print("robotNode: published initial /pose")
+    # print(pose_msg)
 
-        else:
-            #create a subscriber to receive /cmd_vel topic messages:
-            rospy.Subscriber('/cmd_vel',Twist,robotPoseCallback)
-            print("robotNode:created /cmd_vel subscriber")
-        rate.sleep()
-    
+    #create a subscriber to receive /cmd_vel topic messages:
+    rospy.Subscriber('/cmd_vel',Twist,robotPoseCallback)
+    print("robotNode:created /cmd_vel subscriber")
+        
     #keep the node going:
-    # rospy.spin()
+    rospy.spin()
 if __name__ == '__main__':
     try:
         main()
