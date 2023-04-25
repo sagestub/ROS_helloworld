4c996dae6f7c111f51c38aeb6d67248af23c2cca
Sage Stubbs <sagestubbs@gmail.com>
2023-04-25 03:26:03 -0400

    initial catkin build of VO package

---
+++ VO_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sage/Documents/ROS_helloworld/VO_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/sage/pyenvs/ROS544proj/bin:/home/sage/.local/bin:/home/sage/pyenvs/ROS544proj/bin:/home/sage/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/sage/Documents/ROS_helloworld/VO_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sage/Documents/ROS_helloworld/VO_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/sage/Documents/ROS_helloworld/VO_ws/src:$ROS_PACKAGE_PATH"
