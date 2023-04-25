a6bc7639d9e06351f4a44b9528d85866cf9f78d8
sagestub <sagestubbs@gmail.com>
2023-04-25 05:50:58 -0400

    working version

--- VO_ws/voNode.m
+++ VO_ws/voNode.m
@@ -1,10 +1,18 @@
 %% Start ROS
 clear all
 clf
-rosinit
+%% start ros session with matlab node connected to local network master
+masterURI ='http://192.168.1.33:11311' 
+localURI = 'http://192.168.1.9';
+
+rosinit(masterURI)
 %% Initialize the node running VO algorithm on Matlab
-masterHost = 'http://192.168.1.33:11311';
-vo_node = robotics.ros.Node('vo_node', masterHost);
+% nodeName = rosgenmsg('MatlabNode');
+% masterHost = 'http://192.168.1.33:11311';
+suffix = num2str(randi([0,1000]));
+nodeName = ['vo_node','_',suffix]
+
+vo_node = robotics.ros.Node(nodeName);
 
 %% Initialize publisher and subscriber
 %create global handle for callback fuction to use for
@@ -14,3 +22,6 @@ rosmsg_handles.imgSub = robotics.ros.Subscriber(vo_node,'/img',@voCallbackFn);
 
 rosmsg_handles.posePub = robotics.ros.Publisher(vo_node,'/pose','geometry_msgs/Pose');
 rosmsg_handles.posePubmsg = rosmessage(rosmsg_handles.posePub);
+
+rosmsg_handles.markerPub = robotics.ros.Publisher(vo_node,'/marker','visualization_msgs/Marker');
+rosmsg_handles.markerPubmsg = rosmessage(rosmsg_handles.markerPub);
